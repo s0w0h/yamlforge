112ae1125e9d9f886726577980676456b7dd048e
p0w0q <87455543+s0w0h@users.noreply.github.com>
2024-10-18 20:25:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -99,8 +99,8 @@ def resolve_domain_recursive(domain, unique_servers, dns_servers, max_depth=8, d
     resolver = dns.resolver.Resolver()
     resolver.nameservers = dns_servers
     # 设置更短的超时时间
-    resolver.lifetime = 2.0  # 总解析超时时间
-    resolver.timeout = 0.5  # 每个服务器的超时时间
+    resolver.lifetime = 2.5  # 总解析超时时间
+    resolver.timeout = 0.7  # 每个服务器的超时时间
 
     domain_lines = []
     ip_lines = []
